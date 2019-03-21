39c2a945bf496c28c3c13a24b601a502b866d3b6
Vineet John <iamvineetjohn@gmail.com>
2019-03-20 21:08:34 -0700

    Add function that does a GET request

--- src/clojure_questrade/core.clj
+++ src/clojure_questrade/core.clj
@@ -1,6 +1,13 @@
-(ns clojure-questrade.core)
+(ns clojure-questrade.core
+  (:require [clj-http.client :as client]))
 
 (defn iden
   "I don't do a whole lot."
   [x]
   x)
+
+
+(defn example-get
+  "Example GET request"
+  []
+  (client/get "http://example.com"))
--- test/clojure_questrade/core_test.clj
+++ test/clojure_questrade/core_test.clj
@@ -5,3 +5,7 @@
 (deftest a-test
   (testing (iden "abc")
     (is (= "abc"))))
+
+(deftest example-get-test
+  (testing (get (example-get) :status)
+    (is (= 200))))
