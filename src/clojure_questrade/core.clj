37d3ddc565b6264dfe5e6c5f73966d8f95d9f4ad
Vineet John <iamvineetjohn@gmail.com>
2019-03-21 20:20:55 -0700

    Remove dummy identity function

--- src/clojure_questrade/core.clj
+++ src/clojure_questrade/core.clj
@@ -1,11 +1,6 @@
 (ns clojure-questrade.core
   (:require [clj-http.client :as client]))
 
-(defn iden
-  "I don't do a whole lot."
-  [x]
-  x)
-
 
 (defn example-get
   "Example GET request"
--- test/clojure_questrade/core_test.clj
+++ test/clojure_questrade/core_test.clj
@@ -2,9 +2,6 @@
   (:require [clojure.test :refer :all]
             [clojure-questrade.core :refer :all]))
 
-(deftest a-test
-  (testing (iden "abc")
-    (is (= "abc"))))
 
 (deftest example-get-test
   (testing (get (example-get) :status)
