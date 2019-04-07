582f7af88fcbc9bae57e0b9cce7b324eb6a570b5
Vineet John <iamvineetjohn@gmail.com>
2019-04-06 23:24:31 -0700

    Remove test that overwrites the config file
    Not needed anymore, and this will make testing easier

--- test/clojure_questrade/core_test.clj
+++ test/clojure_questrade/core_test.clj
@@ -18,12 +18,6 @@
   (is (not (nil? new-auth-tokens)))
   (is (not (empty? new-auth-tokens))))
 
-(deftest save-auth-tokens-test
-  (save-auth-tokens test-auth-tokens)
-  (def persisted-auth-tokens
-    (ches/parse-stream (clojure.java.io/reader auth-tokens-file-path) true))
-  (is (= persisted-auth-tokens test-auth-tokens)))
-
 (deftest parse-tokens-test
   (def tokens (parse-tokens test-response-body))
   (is (= (get tokens :refresh_token) "xyz"))
